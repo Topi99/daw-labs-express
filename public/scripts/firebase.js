23b92ecb657551ea3fdb23f7ffb5b9547d745dae
Topiltzin <nokoayzack@gmail.com>
2019-04-02 14:57:46 -0600

    Google Login Web Service added

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -29,19 +29,41 @@ Array.prototype.forEach.call( inputs, function( input ) {
 });
 
 document.addEventListener('DOMContentLoaded', e => {
-	document.querySelector('#send').addEventListener('click', e => {
-		const inputFile = document.querySelector('.hiddenFile');
-		const file = inputFile.files[0];
-		const inputURL = document.querySelector('#photoURL');
-		const form = document.querySelector("#sendPic");
-
-		const ref = firebase.storage().ref().child(uuidv4()+'/image');
-		ref.put(file).then(snapshot => {
-			ref.getDownloadURL().then(url => {
-				console.log(url);
-				inputURL.value = url;
-				form.submit();
+	try {
+		document.querySelector('#send').addEventListener('click', ev => {
+			const inputFile = document.querySelector('.hiddenFile');
+			const file = inputFile.files[0];
+			const inputURL = document.querySelector('#photoURL');
+			const form = document.querySelector("#sendPic");
+	
+			const ref = firebase.storage().ref().child(uuidv4()+'/image');
+			ref.put(file).then(snapshot => {
+				ref.getDownloadURL().then(url => {
+					console.log(url);
+					inputURL.value = url;
+					form.submit();
+				})
 			})
-		})
+		});
+	} catch(e) {  }
+
+	document.querySelector('#google').addEventListener('click', async ev => {
+		console.log('Hola');
+		const provider = new firebase.auth.GoogleAuthProvider();
+		
+		const result = await firebase.auth().signInWithPopup(provider);
+		const token = result.credential.accessToken;
+
+		const user = result.user;
+
+		const img = document.createElement('img');
+		img.src = user.photoURL;
+		const p = document.createElement('p');
+		p.innerHTML = user.displayName;
+
+		document.querySelector('.data').appendChild(p);
+		document.querySelector('.data').appendChild(img);
+		
+		console.log("Token", token, "\nUser", user);
 	});
 });
